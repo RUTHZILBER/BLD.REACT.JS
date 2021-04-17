091311e07c40486a75daa70f8f9e1cd29459f228
RuthFromTelAvivIsrael <3120958@gmail.com>
2021-04-18 01:03:52 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// 989 ניתוב הדפים: ברירת מחדל מכאן מנותבות שאר הקומפוננטות
+// 9879 ניתוב הדפים: ברירת מחדל מכאן מנותבות שאר הקומפוננטות
 import React from 'react';
 import { Provider } from 'react-redux';
 import Store from './Store';
